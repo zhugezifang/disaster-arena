40ae4c675a51a9e4db08217a8b22e23ff47eb784
诸葛子房 <xiaoqiu2017wy@163.com>
2024-11-12 15:55:25 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -139,15 +139,15 @@ export default function Header({
           className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href={`/${locale}`} className="-m-1.5 p-1.5" onClick={() => setShowLoadingModal(true)}>
-              <Image className="h-8 w-auto" src="/appicon.svg" alt="sorawebui.com" width={32} height={32}/>
+              <Image className="h-8 w-auto" src="/favicon.svg" alt="Disaster Arena" width={32} height={32}/>
             </a>
             <a href={`/${locale}`} className="-m-1.5 ml-0.5 p-1.5" onClick={() => setShowLoadingModal(true)}>
               <Image
                 className="h-8 w-auto"
-                src="/sorawebui.svg"
+                src="/favicon.svg"
                 width={32}
                 height={24}
-                alt="sorawebui.com"/>
+                alt="Disaster Arena"/>
             </a>
             <button
               type="button"
@@ -161,16 +161,15 @@ export default function Header({
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {navigation.topMenu.map((item) => (
-                  <a
-                    key={item.name}
-                    href={`/${locale}${item.href}`}
-                    onClick={() => setShowLoadingModal(true)}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 "
-                  >
-                    {item.name}
-                  </a>
-                ))}
+
+                <a
+                key={navMenuText.name2}
+                href="/"
+                onClick={() => setShowLoadingModal(true)}
+                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 ">
+                {navMenuText.name2}
+                </a>
+                
               </div>
             </div>
           </div>
